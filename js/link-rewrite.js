bbacb043d8d332fb0dad3ce5b2218eb2193027a5
Alfred-Onuada <aonuada5@gmail.com>
2024-12-23 23:31:12 +0100

    completed project

--- js/link-rewrite.js
+++ js/link-rewrite.js
@@ -1,5 +1,5 @@
 (function rewrite_all_links() {
-  if (window.location.hostname !== '127.0.0.1') {
+  if (window.location.hostname === "alfred-onuada.github.io") {
     const links = document.querySelectorAll('a[href]');
     links.forEach(link => {
       const originalHref = link.getAttribute('href');
